a4a670014f1d57906bffae7c0e2e02cd926cfaa5
Hg Q <hgq287@gmail.com>
2024-11-23 12:35:19 +0700

    * config PostgreSQL via Prisma

---
+++ prisma/migrations/20241123053314_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "image" TEXT,
+    "bio" TEXT,
+    "demo" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
